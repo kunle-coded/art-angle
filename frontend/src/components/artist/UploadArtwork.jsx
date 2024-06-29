1448b021559b8afcf15c54a9fe778422353b97e0
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2024-06-29 04:21:22 +0200

    add display for user inputs to sidebar, add exit and continue buttons

--- frontend/src/components/artist/UploadArtwork.jsx
+++ frontend/src/components/artist/UploadArtwork.jsx
@@ -1,10 +1,75 @@
+import { useField } from "../../hooks/index";
+
 import DropdownInput from "../../ui/DropdownInput";
 import ProgressIndicator from "../../ui/ProgressIndicator";
 import styles from "./UploadArtwork.module.css";
 import Input from "../../ui/Input";
 import ImageUpload from "../../ui/ImageUpload";
+import Button from "../../ui/Button";
+import { useEffect, useState } from "react";
+
+const inputArray = [
+  {
+    id: 0,
+    label: "Title",
+    value: "Nostalgic Beauty",
+  },
+  {
+    id: 1,
+    label: "Category",
+    value: "Contemporary Art",
+  },
+  {
+    id: 2,
+    label: "Subject",
+    value: "Portraiture",
+  },
+  {
+    id: 3,
+    label: "Year",
+    value: "2023",
+  },
+  {
+    id: 4,
+    label: "Medium",
+    value: "Painting",
+  },
+  {
+    id: 5,
+    label: "Material",
+    value: "Canvas",
+  },
+  {
+    id: 6,
+    label: "Styles",
+    value: "Modern, Conceptual",
+  },
+  {
+    id: 7,
+    label: "Dimensions",
+    value: "18 W x 10 H x 1 D in",
+  },
+];
 
 function UploadArtwork() {
+  const [artworkDetails, setArtworkDetails] = useState([]);
+
+  const title = useField("text");
+  const { onReset: resetTitle, ...titleProps } = title;
+
+  function handleSaveContinue(e) {
+    if (title.value) {
+      const titleObj = {
+        id: 0,
+        label: "Title",
+        value: title.value,
+      };
+
+      setArtworkDetails((details) => [...details, titleObj]);
+    }
+    resetTitle(e);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -23,7 +88,14 @@ function UploadArtwork() {
                   <div className={styles.backBtn}>Back to Artworks</div>
                   <div className={styles.sidebarContent}>
                     <div className={styles.imagePlaceholder}></div>
-                    <div className={styles.inputDisplay}>Other contents</div>
+                    <div className={styles.inputDisplay}>
+                      {artworkDetails.map((input) => (
+                        <div key={input.id} className={styles.inputData}>
+                          <h5>{input.label}</h5>
+                          <p>{input.value}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -32,7 +104,11 @@ function UploadArtwork() {
                   <div className={styles.contentHeader}>Artworks</div>
                   <div className={styles.inputItems}>
                     <DropdownInput title="Title">
-                      <Input placeholder="Enter title" size="small" />
+                      <Input
+                        placeholder="Enter title"
+                        size="small"
+                        {...titleProps}
+                      />
                     </DropdownInput>
                     <DropdownInput title="Image">
                       <ImageUpload />
@@ -43,12 +119,20 @@ function UploadArtwork() {
             </div>
             {/* buttons */}
             <div className={styles.progressBtnWrapper}>
-              <div className={styles.progressBtns}>
-                <p>Save & Exit</p>
-                <p>Continue</p>
+              <div className={styles.uploadAreaGrid}>
+                <div className={styles.sidebarColumn}></div>
+                <div className={styles.contentColumn}>
+                  <div className={styles.progressBtns}>
+                    <Button type="secondary" size="small">
+                      Save & Exit
+                    </Button>
+                    <Button size="small" onClick={handleSaveContinue}>
+                      Save & Continue
+                    </Button>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className={styles.wrap}></div>
           </div>
         </div>
       </div>
--- frontend/src/ui/Input.jsx
+++ frontend/src/ui/Input.jsx
@@ -1,6 +1,6 @@
 import styles from "./Input.module.css";
 
-function Input({ placeholder = "", label = "", size = "" }) {
+function Input({ placeholder = "", label = "", size = "", ...props }) {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -11,6 +11,7 @@ function Input({ placeholder = "", label = "", size = "" }) {
             size === "small" ? styles.small : ""
           }`}
           style={{ paddingRight: "33px" }}
+          {...props}
         />
         {label && (
           <div className={styles.inputLabel}>
